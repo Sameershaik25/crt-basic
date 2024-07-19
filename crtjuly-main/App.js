acc46d89e1b2b5cff880792a063f4f7e89a1c179
Sameershaik25 <shaiksameershaik99@gmail.com>
2024-07-19 18:36:34 +0530

    Add files via upload

---
+++ crtjuly-main/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ return (
+	<div>{
+		(10<5)&& alert('This alert will be shown')
+		}
+		</div>
+ );
+}
+
+export default App;
